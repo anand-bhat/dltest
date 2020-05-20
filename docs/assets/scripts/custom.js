0f817957d8471cb86392d65521cf1d306869c4b7
Anand Bhat <anand-bhat@users.noreply.github.com>
2020-05-20 17:01:18 +1000

    T

--- docs/assets/scripts/custom.js
+++ docs/assets/scripts/custom.js
@@ -42,41 +42,25 @@ function projectProgressChart(project, labels, values) {
 		type: 'line',
 		data: {
 			labels: labels,
-			datasets: [{label: project, data: values, fill: false, backgroundColor: #9c2ca3, borderColor: #000000}]
+			datasets: [{label: project, data: values, fill: false, backgroundColor: '#9c2ca3', borderColor: '#000000'}]
 		},
 		options: {
 			//responsive: true, // Instruct chart js to respond nicely.
-			maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height
-			//datasets : [{
-			//	line: {
-			//		showLine: true
-			//	}
-			//}],
+			maintainAspectRatio: false,
 			legend: {
 				display: false,
 			},
 			scales: {
 				xAxes: [{
-				//	gridLines: {
-				//		display: false,
-				//	},
 					scaleLabel : {
 						display: true,
-						labelString: "Date"
-					},
-				//	ticks: {
-				//		stepSize: 1,
-				//		max: maxClonesPerRun-1
-				//	}
+						labelString: 'Date'
+					}
 				}],
 				yAxes: [{
 					scaleLabel : {
 						display: true,
 						labelString: 'Progress (%)'
-					//},
-					//ticks: {
-						//max: 100,
-						//min: 0
 					}
 				}]
 			},
