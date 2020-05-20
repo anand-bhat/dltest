259730d9cb136d0ba9d78a86152622a35ba907a3
Anand Bhat <anand-bhat@users.noreply.github.com>
2020-05-20 11:30:21 +1000

    T

--- docs/assets/scripts/custom.js
+++ docs/assets/scripts/custom.js
@@ -36,66 +36,15 @@ function getProgressBar(percentage, color) {
 	return `<div class="progress"><div class="progress-bar role="progressbar" style="width: ${percentage}%; background-color: ${color}" aria-valuenow="${percentage}" aria-valuemin="0" aria-valuemax="100">${percentage}%</div></div>`;
 }
 
-function prcg2Chart(projectId, runId, maxClonesPerRun, maxGensPerClone, dataSeries) {
+function projectProgressChart(projectId, labels, data) {
 	'use strict';
-	var myChart = new Chart($('#prcg2Chart'), {
-		type: 'scatter',
+	var myChart = new Chart($('#projectDetailsChart'), {
+		type: 'line',
 		data: {
-			datasets: dataSeries,
-		},
-		options: {
-				//responsive: true, // Instruct chart js to respond nicely.
-				maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height
-			datasets : [{
-				line: {
-					showLine: true
-				}
-			}],
-			legend: {
-				display: false,
-			},
-			scales: {
-				xAxes: [{
-					gridLines: {
-						display: false,
-					},
-					scaleLabel : {
-						display: true,
-						labelString: "Clone #"
-					},
-					ticks: {
-						stepSize: 1,
-						max: maxClonesPerRun-1
-					}
-				}],
-				yAxes: [{
-					scaleLabel : {
-						display: true,
-						labelString: "Gen #"
-					},
-					ticks: {
-						max: maxGensPerClone
-					}
-				}]
-			},
-			title: {
-				display: true,
-				text: 'Progress for Project: ' + projectId + '; Run: ' + runId
-			},
-			tooltips: {
-				callbacks: {
-					label: function(tooltipItem, data) {
-						// Returned formatted tooltip
-						return 'Clone: ' + tooltipItem.xLabel + '; Gen: ' + tooltipItem.yLabel + ' (' + ((tooltipItem.yLabel/maxGensPerClone) * 100) + '%)';
-					}
-				},
-				filter: function (tooltipItem, data) {
-					// Hide tooltip for first coordinate
-					return !(tooltipItem.index == 0);
-				}
-			}
+			labels: labels
+			datasets: [{label: 'My First Dataset', data: data, fill: false,borderColor:rgb(75, 192, 192), lineTension: 0.1}],
 		}
-	});
+});
 
 	return myChart;
 }
@@ -105,9 +54,14 @@ function projectDetailsLink(project, run) {
 	return `<div><a href="./projectDetails?project=${project}">${project}</a></div>`;
 }
 
+function datasourceLink(sheetIndex) {
+	'use strict';
+	return `https://spreadsheets.google.com/feeds/list/16HhDEP6eG9sxX0yZd0NbLMgNAjafz_ms88lGUytV6EI/${sheetIndex}/public/full?alt=json`;
+}
+
 function projects() {
 	'use strict';
-	$.getJSON('https://spreadsheets.google.com/feeds/list/16HhDEP6eG9sxX0yZd0NbLMgNAjafz_ms88lGUytV6EI/1/public/full?alt=json')
+	$.getJSON(datasourceLink(1))
 	.done(function(data) {
 		var dataRows = [];
 		var progress = 0.0;
@@ -115,13 +69,13 @@ function projects() {
 		$.each(data.feed.entry, function(index, row) {
 			progress = row.gsx$progress.$t.replace('%','');
 			colorClassIndex = Math.max(0, Math.floor((30 * progress) / 100) - 1);
-			dataRows[index] = { projectVal: row.gsx$project.$t, project: projectDetailsLink(row.gsx$project.$t), progressVal: progress, progress: getProgressBar(progress, colorClass[colorClassIndex]), average: row.gsx$dayaveragechange.$t, daysToCompletionVal: row.gsx$estimateddaystocompletion.$t, daysToCompletion: row.gsx$estimatedcompletion.$t, completionDate: row.gsx$estimatedcompletiondate.$t };
+			dataRows[index] = { projectVal: row.gsx$project.$t, project: projectDetailsLink(row.gsx$sheetindex.$t), progressVal: progress, progress: getProgressBar(progress, colorClass[colorClassIndex]), average: row.gsx$dayaveragechange.$t, daysToCompletionVal: row.gsx$estimateddaystocompletion.$t, daysToCompletion: row.gsx$estimatedcompletion.$t, completionDate: row.gsx$estimatedcompletiondate.$t, sheetIndex: row.gsx$sheetindex.$t };
 		});
 		$('#projectSummaryTable').bootstrapTable({data: dataRows, formatNoMatches: function () {return 'No data found.';}});
 		$('#projectSummaryTable').show();
 
-		var dateString = new Date(Date.parse(data.feed.updated.$t)).toLocaleString(undefined, { dateStyle: 'full', timeStyle: 'long' });
-		$('#projectSummaryTitle').html('Last updated at ' + dateString);
+		//var dateString = new Date(Date.parse(data.feed.updated.$t)).toLocaleString(undefined, { dateStyle: 'full', timeStyle: 'long' });
+		//$('#projectSummaryTitle').html('Last updated at ' + dateString);
 	})
 	.fail(function(data) {
 		// The project specified in the URL does not point to a valid project or there isn't data yet
@@ -136,11 +90,7 @@ function projectDetails() {
 	'use strict';
 	var urlParams = new URLSearchParams(window.location.search);
 	if (!urlParams.has('project')) {
-		alert('Missing project ID');
-		return;
-	}
-	if (!urlParams.has('run')) {
-		alert('Missing run ID');
+		alert('Missing project.');
 		return;
 	}
 
@@ -150,56 +100,31 @@ function projectDetails() {
 		return;
 	}
 
-	var runId = urlParams.get('run');
-	if (!Number.isInteger(parseInt(runId))) {
-		alert('Unable to get data for Project: ' + projectId + '; Run: ' + runId);
-		return;
-	}
-
 	projectId = parseInt(projectId);
-	runId = parseInt(runId);
 
-	$.getJSON("../assets/data/" + projectId + ".json")
+	$.getJSON(datasourceLink(projectId))
 	.done(function(data) {
-		var runData = data.runs.find(run=>run.run==runId);
-		if (!runData) {
-			alert('Unable to get data for Project: ' + projectId + '; Run: ' + runId);
-			return;
-		}
-
-		$('#prcg2Title').html('Progress for Project: ' + projectId + '; Run: ' + runId + '. Last updated at ' + new Date(data.lastUpdated).toLocaleString());
-
 		var dataSeries = [];
 		var dataRows = [];
-		var totalGensCompleted = 0;
 		var percentage = 0.0;
 		var colorClassIndex = '';
-		$.each(runData.clones, function(index, clone) {
-			var genCount = clone.gen + 1;
-			colorClassIndex = Math.max(0, Math.floor((30 * genCount) / data.maxGensPerClone) - 1);
-			percentage =  Math.round((((100 * genCount) / data.maxGensPerClone) + Number.EPSILON) * 100) / 100;
-			dataSeries[index] = { data: [{x: clone.clone, y: 0}, {x: clone.clone, y: Math.max(0, clone.gen)}], borderColor: colorClass[colorClassIndex], backgroundColor:colorClass[colorClassIndex] };
-			dataRows[index] = { clone: clone.clone, gen: clone.gen === -1 ? '-':clone.gen, remaining: (data.maxGensPerClone - genCount), progress: getProgressBar(percentage, colorClass[colorClassIndex]) };
-			totalGensCompleted += genCount;
-		});
-		prcg2Chart(projectId, runId, data.maxClonesPerRun, data.maxGensPerClone, dataSeries);
-		$('#prcg2Table').bootstrapTable({data: dataRows, formatNoMatches: function () {return 'No data found.';}});
-		var totalGensForRun = data.maxClonesPerRun * data.maxGensPerClone;
-		colorClassIndex = Math.max(0, Math.floor((30 * totalGensCompleted) / totalGensForRun) - 1);
-		percentage = Math.round((((100 * totalGensCompleted) / totalGensForRun) + Number.EPSILON) * 100) / 100;
-		$('#prcg2ProgressBar').html(getProgressBar(percentage, colorClass[colorClassIndex]));
-		$('#prcg2Table').show();
-		$('#prcg2UpToProject').show();
-		$('#prcg2UpToProjectURL').attr('href', './prcgProgress?project=' + projectId);
+  // Map JSON labels  back to values array
+  var labels = data.feed.entry.map(function (e) {
+    return e.gsx$date.$t;
+  });
+      
+  // Map JSON values back to values array
+  var values = data.feed.entry.map(function (e) {
+    return e.gsx$completed.$t.replace('%','');
+  });
 
-		if (runId != 0) {
-			$('#prcg2PreviousRun').show();
-			$('#prcg2PreviousRunURL').attr('href', './prcgProgress2?project=' + projectId + '&run=' + (runId-1));
-		}
-		if (runId != data.maxRuns-1) {
-			$('#prcg2NextRun').show();
-			$('#prcg2NextRunURL').attr('href', './prcgProgress2?project=' + projectId + '&run=' + (runId+1));
-		}
+		projectProgressChart(projectId, labels, values);
+		//$('#prcg2Table').bootstrapTable({data: dataRows, formatNoMatches: function () {return 'No data found.';}});
+		//var totalGensForRun = data.maxClonesPerRun * data.maxGensPerClone;
+		//colorClassIndex = Math.max(0, Math.floor((30 * totalGensCompleted) / totalGensForRun) - 1);
+		//percentage = Math.round((((100 * totalGensCompleted) / totalGensForRun) + Number.EPSILON) * 100) / 100;
+		//$('#prcg2ProgressBar').html(getProgressBar(percentage, colorClass[colorClassIndex]));
+		//$('#prcg2Table').show();
 	})
 	.fail(function(data) {
 		// The project specified in the URL does not point to a valid project or there isn't data yet
